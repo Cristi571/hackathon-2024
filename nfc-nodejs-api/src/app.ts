5db1b9cbdbdd47580bb00fd44783d81560b2e98c
Cristian Tirche <cristian.tirche@gmail.com>
2024-07-03 18:24:53 +0200

    Updates

--- nfc-nodejs-api/src/app.ts
+++ nfc-nodejs-api/src/app.ts
@@ -1,18 +1,12 @@
 import express from 'express';
-<<<<<<< HEAD
 import routes from './routes'; // Import the main router from the routes directory
 import { logger, notFoundHandler, errorHandler } from './middlewares/logger'; // Import the logger and error-handling middlewares
-=======
-import userRoutes from './routes/userRoutes';
-import nfcRoutes from './routes/nfcRoutes';
->>>>>>> 1cb9da7403449d08a044c7cb0e7b24bf02c50285
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-<<<<<<< HEAD
 
 app.use(routes);
 // Use the logger middleware
@@ -22,10 +16,5 @@ app.use(notFoundHandler);
 // Handle other errors
 app.use(errorHandler);
 
-=======
-// Define routes
-app.use('/api/users', userRoutes);
-app.use('/api/nfc', nfcRoutes);
->>>>>>> 1cb9da7403449d08a044c7cb0e7b24bf02c50285
 
 export default app;
--- nfc-nodejs-api/src/routes/userRoutes.ts
+++ nfc-nodejs-api/src/routes/userRoutes.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import { addUser } from '../controllers/userController';
+import { getAllUsers, createUser } from '../controllers/userController';
 
 const router = Router();
 
-<<<<<<< HEAD
 router.get('/list', getAllUsers);
 router.post('/create', createUser);
-=======
-router.post('/add-user', addUser);
->>>>>>> 1cb9da7403449d08a044c7cb0e7b24bf02c50285
 
 export default router;
