93fa17572886c5f64cfcb05632d495a594d4cdf7
CristianTirche <cristian.tirche@estiam.com>
2024-07-04 13:23:23 +0200

    Feat: Adding CORS

--- nfc-nodejs-api/src/app.ts
+++ nfc-nodejs-api/src/app.ts
@@ -1,8 +1,8 @@
+
 import express from 'express';
 import routes from './routes'; // Import the main router from the routes directory
 // Import the logger and error-handling middlewares
 import { logger, notFoundHandler, errorHandler } from './middlewares/logger'; 
-
 const app = express();
 
 // Middleware to parse JSON bodies
--- nfc-nodejs-api/src/server.ts
+++ nfc-nodejs-api/src/server.ts
@@ -1,15 +1,28 @@
 import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
 import app from './app';
 import connectDB from './config/db';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Connect to the database
 connectDB();
 
+const server = express();
+
+// Middleware to parse JSON bodies
+server.use(express.json());
+
+// CORS configuration
+server.use(cors()); // Allow all origins - you can configure specific origins if needed
+
+// Use the app as a middleware
+server.use(app);
+
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
